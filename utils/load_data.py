1845e9dc960f87adcbf9604f9d00b399a63915f9
JoseOrdonezB <ord231329@uvg.edu.gt>
2026-03-21 13:06:13 -0600

    fix: cargar laberinto desde txt

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,31 +1,17 @@
-import pandas as pd
+def load_maze(file_path):
+    maze = []
+    start_positions = []
+    goal_positions = []
 
-def load_costs(path):
-    df = pd.read_excel(path)
+    with open(file_path, 'r') as file:
+        for i, line in enumerate(file):
+            row = list(line.strip())
+            maze.append(row)
 
-    graph = {}
+            for j, value in enumerate(row):
+                if value == '2':
+                    start_positions.append((i, j))
+                elif value == '3':
+                    goal_positions.append((i, j))
 
-    for _, row in df.iterrows():
-        origin = row['Origen']
-        destination = row['Destino']
-        cost = row['Cost']
-
-        if origin not in graph:
-            graph[origin] = {}
-
-        graph[origin][destination] = cost
-
-    return graph
-
-def load_heuristics(path):
-    df = pd.read_excel(path)
-
-    heuristic = {}
-
-    for _, row in df.iterrows():
-        node = row['Activity']
-        value = row['Recovery time after burning 300cal (minutes)']
-
-        heuristic[node] = value
-
-    return heuristic
+    return maze, start_positions, goal_positions
